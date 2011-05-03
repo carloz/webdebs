c6bab423e0d72d8076759b22ff76d11aae6cdfdd
Dharma Ferrari <dharma@MrGray.local>
2011-05-03 23:46:29 +0200

    Super alfa

--- js/start.js
+++ js/start.js
@@ -4,41 +4,41 @@ $(document).ready(function() {
     $.getJSON('content/events.json', function(data) 
     {
       updateDomEvents(data)
-       
     }
-    ).error(function(XMLHttpRequest, textStatus, errorThrown) { console.log(textStatus, errorThrown )});
+    ).error(function(XMLHttpRequest, textStatus, errorThrown) { debug(textStatus, errorThrown); });
 
 //meetings
     
     
-    $.getJSON('content/meetings.json', function(data){ updateDomMeetings(data)});
+    $.getJSON('content/meetings.json', function(data){ updateDomMeetings(data); });
     
     
     function updateDomEvents(data)
     {
       $.each(data['events'], 
-          function(index, value)
-          {
-            $('#events').append('<div id="event_' + index +'"><p>' + value.name +'</p><p>' + value.www +'</p> <p>' + value.date + '</p> <hr /></div>')
+        function(index, value) {
+          if (value.status=="visible") {
+            $('#events .data').append('<div id="event_' + index +'" class="event trentatre"><p>' + value.name +'</p><p>' + value.url +'</p> <p>' + value.date + '</p></div>');
           }
-        );
-    }
+        });
+    };
     
     
     
     function updateDomMeetings(data)
     {
       $.each(data['meetings'], function(index, value){
-          $('#meetings').append('<div id="meeting_' + index +'"><p>' + value.title +'</p><p>' + value.www +'</p> <p>' + value.date + '</p> <hr /></div>')
-        }
-        );
-      console.log('pippo');
-      console.log(data);
-    }
-    
+        $('#meetings .data').append('<div id="meeting_' + index +'" class="meeting trentatre"><p>' + value.title +'</p><p>' + value.url +'</p> <p>' + value.date + '</p></div>');
+      });
+      debug(data);
+    };
 
 });
 
 
 
-
+function debug($obj) {
+  if (window.console && window.console.log) {
+    window.console.log($obj);
+  }
+}
