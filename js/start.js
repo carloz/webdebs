ff6838c97eeba3051073cb7c451397d843a61fe3
cirpo <alessandro.cinelli@gmail.com>
2011-06-05 19:53:56 +0200

    done model layout and slider

--- js/start.js
+++ js/start.js
@@ -1,4 +1,4 @@
-ify=function(){var entities={'"':'"','&':'&','<':'<','>':'>'};return{"link":function(t){return t.replace(/[a-z]+:\/\/[a-z0-9-_]+\.[a-z0-9-_:~%&\?\/.=]+[^:\.,\)\s*$]/ig,function(m){return'<a href="'+m+'">'+((m.length>25)?m.substr(0,24)+'...':m)+'</a>';});},"at":function(t){return t.replace(/(^|[^\w]+)\@([a-zA-Z0-9_]{1,15})/g,function(m,m1,m2){return m1+'@<a href="http://twitter.com/'+m2+'">'+m2+'</a>';});},"hash":function(t){return t.replace(/(^|[^\w'"]+)\#([a-zA-Z0-9_]+)/g,function(m,m1,m2){return m1+'#<a href="http://search.twitter.com/search?q=%23'+m2+'">'+m2+'</a>';});},"clean":function(tweet){return this.hash(this.at(this.link(tweet)));}};}();
+var ify=function(){var entities={'"':'"','&':'&','<':'<','>':'>'};return{"link":function(t){return t.replace(/[a-z]+:\/\/[a-z0-9-_]+\.[a-z0-9-_:~%&\?\/.=]+[^:\.,\)\s*$]/ig,function(m){return'<a href="'+m+'">'+((m.length>25)?m.substr(0,24)+'...':m)+'</a>';});},"at":function(t){return t.replace(/(^|[^\w]+)\@([a-zA-Z0-9_]{1,15})/g,function(m,m1,m2){return m1+'@<a href="http://twitter.com/'+m2+'">'+m2+'</a>';});},"hash":function(t){return t.replace(/(^|[^\w'"]+)\#([a-zA-Z0-9_]+)/g,function(m,m1,m2){return m1+'#<a href="http://search.twitter.com/search?q=%23'+m2+'">'+m2+'</a>';});},"clean":function(tweet){return this.hash(this.at(this.link(tweet)));}};}();
 
 TwitterAPI = {
 
@@ -12,142 +12,120 @@ TwitterAPI = {
 
 $(document).ready(function() {
 
-  
-console.log($('li.centerEvent').width());
-console.log($('#event_block').width());
 
-var startp = ($('#event_block').width() - $('li.centerEvent').width())/2 ;
-//var endp = ;
+  //colorbox google calendar
+  $(".calendar").colorbox({width:"80%", height:"80%", iframe:true});
+  $(".map").colorbox({width:"80%", height:"80%", iframe:true});
 
-console.log(startp);
+  var event_block = $('#event_block');
+  var event_list = $('#event_list');
+  var meeting_block = $('#meeting_block');
+  var meeting_list = $('#meeting_list');
+  var centered_event = $('li.centered_event');
+  var centered_meeting = $('li.centered_meeting');
+  var distance_event_to_center_from_event_list_container =  0 - centered_event.position().left;
+  var distance_meeting_to_center_from_meeting_list_container =  0 - centered_meeting.position().left;
+  var extra_space_between_visual_area_and_event_to_center = centered_event.width() - event_block.width();
+  var extra_space_between_visual_area_and_meeting_to_center = centered_event.width() - event_block.width();
 
-$('#event_block ul').css('top',$('li.centerEvent').offset() - startp +'px')
+  //move .centered_event slide in the center
+  $('#event_block ul').css('left', (distance_event_to_center_from_event_list_container - (extra_space_between_visual_area_and_event_to_center /2)) +'px')
 
-//twitter
-TwitterAPI.statuses.user_timeline('webdebresa',5,function(json){
-  var content = "";
-  content += '<ul>';
-  $.each(json, function(i){
-    var tweet = ify.clean(this['text']);
-    var d = new Date(this['created_at']);
-    var date_show = d.getDate()+'/'+d.getMonth()+'/'+d.getFullYear()+' '+d.getHours()+':'+d.getMinutes();
-    content += '<li>' + tweet + ' (<date>'+date_show+'</date>)</li>';
-  });
-  content += '</ul>';
-  $('aside').append(content);
-});
-  
-  
-//events
-    $.getJSON('content/events.json', function(data) 
-    {
-      updateDomEvents(data)
+
+  $('#next_event').click(
+    function(e) {
+      e.preventDefault();
+      if(centered_event.next().length != 0){
+        centerNewEvent(centered_event.next());
+      }
     }
-    ).error(function(XMLHttpRequest, textStatus, errorThrown) {debug(textStatus, errorThrown);});
+  );
 
-//meetings
-    
-    
-    $.getJSON('content/meetings.json', function(data){updateDomMeetings(data);});
-    
-    
-    function updateDomEvents(data)
-    {
-      $.each(data['events'], 
-        function(index, value) {
-          if (value.status=="visible") {
-            $('#events .data').append('<div id="event_' + index +'" class="event trentatre"><p>' + value.name +'</p><p>' + value.url +'</p> <p>' + value.date + '</p></div>');
-          }
-        });
-    };
-    
-    
-    
-    function updateDomMeetings(data)
-    {
-      $.each(data['meetings'], function(index, value){
-        $('#meetings .data').append('<div id="meeting_' + index +'" class="meeting trentatre"><p>' + value.title +'</p><p>' + value.url +'</p> <p>' + value.date + '</p></div>');
-      });
-      
-      $('#meetings .data div:even').css('background','#eee');
-      
-      debug(data);
-    };
-    
-    
-    $('#next').click(
-        function(e)
-        {
-          e.preventDefault();
-          var p = '' + $('#event_block ul').position().left - 50 + '';
+  function centerNewEvent(event_to_be_centered )
+  {
+    event_to_be_centered.addClass('centered_event');
+    centered_event.removeClass('centered_event');
+    centered_event = event_to_be_centered;
 
-          $('ul').css('left',   p + 'px');
-        }
+    var distance_event_to_center_from_event_list_container = 0 - centered_event.position().left;
 
-    );
-      
-    var intervalKey;
+    var extra_space_between_visual_area_and_event_to_center = centered_event.width() - event_block.width();
 
-    $('#next').hover(
-      function(e)
+     $('#event_block ul').animate(
       {
-        e.preventDefault();
-        var move = function(){ 
-            var p = '' + $('#event_block ul').position().left - 50 + '';
-            $('ul').css('left',   p + 'px');
-        }
-        
-       intervalKey = setInterval(move, 300);          
+        'left': (distance_event_to_center_from_event_list_container - (extra_space_between_visual_area_and_event_to_center /2)) +'px'
       },
-      
-      function(){
-       clearInterval(intervalKey);
-      }
-
-    );
-      
-      
-     $('#prev').hover(
-      function(e)
-      {
-        e.preventDefault();
-        over = true;
-        var move = function(){ 
-          if(over){
-            var p = '' + (parseInt($('#event_block ul').position().left) + 50) + '';
-            $('ul').css('left',   p + 'px');
-          }
-        }
-        
-       intervalKey = setInterval(move, 1300);          
-      },
-      
-      function(){
-       clearInterval(intervalKey);
-      }
-
-    );
-      
-    $('#prev').click(
-      function(e)
-        {
-         e.preventDefault();
-          console.log($('#event_block ul').position().left);
-          var p = '' + (parseInt($('#event_block ul').position().left) + 50) + '';
-
-          $('ul').css('left',   p + 'px');
-          console.log(p);
-          
-        }
-  
-    );
-
-});
-
-
-
-function debug($obj) {
-  if (window.console && window.console.log) {
-    window.console.log($obj);
+      'slow',
+      'linear'
+     );
   }
-}
+
+   $('#prev_event').click(
+    function(e){
+      e.preventDefault();
+      if(centered_event.prev().length != 0){
+          centerNewEvent(centered_event.prev());
+      }
+
+     }
+  );
+    
+  //meetings
+  $('#meeting_block ul').css('left', (distance_meeting_to_center_from_meeting_list_container - (extra_space_between_visual_area_and_meeting_to_center /2)) +'px')
+
+
+  $('#next_meeting').click(
+    function(e) {
+      e.preventDefault();
+      if(centered_meeting.next().length != 0){
+        centerNewMeeting(centered_meeting.next());
+      }
+    }
+  );
+
+  function centerNewMeeting(meeting_to_be_centered )
+  {
+    meeting_to_be_centered.addClass('centered_meeting');
+    centered_meeting.removeClass('centered_meeting');
+    centered_meeting = meeting_to_be_centered;
+
+    var distance_meeting_to_center_from_meeting_list_container = 0 - centered_meeting.position().left;
+
+    var extra_space_between_visual_area_and_meeting_to_center = centered_meeting.width() - meeting_block.width();
+
+     $('#meeting_block ul').animate(
+      {
+        'left': (distance_meeting_to_center_from_meeting_list_container - (extra_space_between_visual_area_and_meeting_to_center /2)) +'px'
+      },
+      'slow',
+      'linear'
+     );
+  }
+
+   $('#prev_meeting').click(
+    function(e){
+      e.preventDefault();
+      if(centered_meeting.prev().length != 0){
+          centerNewMeeting(centered_meeting.prev());
+      }
+
+     }
+  );
+    
+
+  //twitter
+  TwitterAPI.statuses.user_timeline('webdebresa',5,function(json){
+    var content = "";
+    content += '<ul>';
+    $.each(json, function(i){
+      var tweet = ify.clean(this['text']);
+      var d = new Date(this['created_at']);
+      var date_show = d.getDate()+'/'+d.getMonth()+'/'+d.getFullYear()+' '+d.getHours()+':'+d.getMinutes();
+      content += '<li>' + tweet + ' (<date>'+date_show+'</date>)</li>';
+    });
+    content += '</ul>';
+    $('aside').append(content);
+  });
+
+      
+});
